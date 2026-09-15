71164f0ca0357bead248b2451ec7c6e669bd3b21
abdullah3lharbi10-design <abdullah.3lharbi10@gmail.com>
2026-09-15 23:09:45 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>منصة نبراس القدرات 🚀</h1>
+      <p>الموقع يعمل بنجاح!</p>
+    </main>
+  );
+}
